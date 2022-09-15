ca61f70ae0c96ee9875381219200cceca957636d
optdxf <113573841+optdxf@users.noreply.github.com>
2022-09-14 21:30:10 -0700

    Delete road_rager directory

--- road_rager/physics/physics_constants.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PHYSICS_CONSTANTS_H__
-#define __PHYSICS_CONSTANTS_H__
-
-/**
-* Possible body types for objects in the game.
-*/
-typedef enum body_type { CAR, WALL, GRASS, FINISH_BEGIN, FINISH_END, TRACK } body_type_t;
-
-#endif
